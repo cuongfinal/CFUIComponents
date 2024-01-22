d0f79c04660f1e483abc582da020facc95ac3478
CuongLQT <cuonglqt@mti-tech.vn>
2024-01-22 11:28:00 +0700

    Project Init

---
+++ Crooti-New/Crooti-New/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Crooti-New
+//
+//  Created by Le Quang Tuan Cuong(CuongLQT) on 22/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Crooti-New/Crooti-New/Crooti_NewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crooti_NewApp.swift
+//  Crooti-New
+//
+//  Created by Le Quang Tuan Cuong(CuongLQT) on 22/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Crooti_NewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
